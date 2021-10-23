6797bedd33da2841bf3a48390658eb84ad16ac2c
MichaelD002 <89713959+MichaelD002@users.noreply.github.com>
2021-10-23 15:53:02 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,44 +1,57 @@
-const $video = document.querySelector('#video');
-const $play = document.querySelector('#play');
-const $pause = document.querySelector('#pause');
-const $backward = document.querySelector('#backward');
-const $forward = document.querySelector('#forward');
-$play.addEventListener ('click', handlePlay);
-$pause.addEventListener ('click', handlePause);
-$backward.addEventListener ('click', handleBackward);
-$forward.addEventListener ('click', handleForward);
-function handlePlay(){
-    $video.play();
-    $play.hidden = true;
-    $pause.hidden = false;
-    console.log('Diste click al boton play');
-}
-function handlePause(){
-    $video.pause();
-    $pause.hidden = true;
-    $play.hidden = false;
-    console.log('Diste click al boton pausa');
-}
-function handleBackward(){
-    $video.currentTime = $video.currentTime - 10;
-    console.log('Diste click al boton retroceder 10 segundos',$video.currentTime);
-}
-function handleForward(){
-    $video.currentTime = $video.currentTime+ 10;
-    console.log('Diste click al boton adelnatar 10 segundos',$video.currentTime);
-}
-const $progress = document.querySelector('#progress');
-$video.addEventListener ('loadedmetadata', handleLoaded);
-$video.addEventListener ('timeupdate', handleTimeupdate);
-function handleLoaded(){
-    $progress.max = $video.duration;
-    console.log('ha cargado mi video',$video.duration);
-}
-function handleTimeupdate(){
-    $progress.value= $video.currentTime;
-}
-$progress.addEventListener('input', handleinput);
-function handleinput(){
-    $video.currentTime = $progress.value;
-    console.log($progress.value)
-}
+const $video = document.querySelector('#video')
+    const $play = document.querySelector('#play')
+    const $pause = document.querySelector('#pause')
+    const $backward = document.querySelector('#backward')
+    const $forward = document.querySelector('#forward')
+
+    $play.addEventListener('click', handlePlay )
+    $pause.addEventListener('click', handlePause )
+    
+    function handlePlay() {
+        $video.play()
+        $play.hidden = true
+        $pause.hidden = false
+console.log('le diste click al botón de play')
+    }
+
+    function handlePause() {
+        $video.pause()
+        $pause.hidden = true
+        $play.hidden = false
+console.log('le diste click al botón de pausa')
+    }
+
+
+
+    $backward.addEventListener('click', handleBackward)
+
+   function handleBackward() {
+   $video.currentTime = $video.currentTime - 10
+   console.log('para atrás 10 segundos', $video.currentTime)
+   }
+   $forward.addEventListener('click', handleForward )
+
+   function handleForward() {
+   $video.currentTime = $video.currentTime + 10
+   console.log('para adelante 10 segundos', $video.currentTime) 
+   }
+
+const $progress = document.querySelector('#progress')
+   $video.addEventListener('loadedmetadata', handleLoaded)
+   $video.addEventListener('timeupdate', handleTimeUpdate)
+   function handleLoaded(){
+       $progress.max = $video.duration
+       console.log('ha cargado mi video', $video.duration)
+   }
+
+   function handleTimeUpdate() {
+     $progress.value = $video.currentTime
+//console.log('tiempo actual', $video.currentTime)//
+   }
+
+   $progress.addEventListener('input', handleInput)
+
+   function handleInput() {
+    $video.currentTime = $progress.value
+       console.log($progress.value)
+   }
